f39f5be69451e37738330b78ec872d18c4883c7d
Arthur Ribeiro Chaves <103151876+ArthurHRChaves@users.noreply.github.com>
2025-04-23 20:10:43 -0300

    Feature: Implementando classe faturaCliente

--- UnaPocAtividade/src/faturaCliente/FaturaCliente.java
+++ UnaPocAtividade/src/faturaCliente/FaturaCliente.java
@@ -1,5 +1,49 @@
 package faturaCliente;
 
 public class FaturaCliente {
+    private int id;
+    private Cliente cliente;
+    private double valor;
 
+    public FaturaCliente(int id, Cliente cliente, double valor) {
+        this.id = id;
+        this.cliente = cliente;
+        this.valor = valor;
+    }
+
+    public FaturaCliente(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValorComDesconto() {
+        if (cliente != null) {
+            return valor - (valor * cliente.getDesconto() / 100.0);
+        }
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Fatura [id=" + id + ", cliente=" + cliente.toString() + ", valor=" + valor + "]";
+    }
 }
